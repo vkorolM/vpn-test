3364f49af2482ade14df74c5da0ce40e882fb906
vkorolM <vkorolM@mozilla.com>
2023-06-20 23:34:45 -0400

    fix branch

---
+++ src/test/java/Core/BaseTest.java
@@ -0,0 +1,42 @@
+package Core;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.net.URL;
+
+public class BaseTest {
+
+		protected AppiumDriver driver;
+		private static String appiumUrl = "http://127.0.0.1:4723";
+
+		@BeforeClass
+		protected void setUp() throws Exception {
+				DesiredCapabilities capabilities = new DesiredCapabilities();
+				capabilities.setCapability("platformName", "Android");
+				capabilities.setCapability("deviceName", "appium");
+				capabilities.setCapability("platformVersion", "13");
+				capabilities.setCapability("automationName", "uiautomator2");
+				capabilities.setCapability("printPageSourceOnFindFailure", "true");
+				capabilities.setCapability("app", "/Users/vladyslavkorol/Developement/Mozilla/vpn-automation/mozillavpn-x86-release.apk");
+
+				UiAutomator2Options uiAutomator2Options = new UiAutomator2Options()
+						.setAutomationName("UiAutomator2")
+						.setDeviceName("Pixel3")
+						.setPlatformVersion("11")
+						.setPlatformName("Android")
+						.setPrintPageSourceOnFindFailure(true)
+						.setApp("/Users/vladyslavkorol/Developement/Mozilla/vpn-automation/vpn/mozillavpn-x86-release.apk");
+
+				driver = new AndroidDriver(new URL(appiumUrl), uiAutomator2Options);
+		}
+
+		@AfterClass
+		protected void tearDown() throws Exception {
+				driver.quit();
+		}
+}
---
+++ src/test/java/Tests/POCTests.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import Core.BaseTest;
+import org.testng.annotations.Test;
+
+public class POCTests extends BaseTest {
+
+		@Test
+		public void firstTest() {
+				System.out.println("Test");
+		}
+}
