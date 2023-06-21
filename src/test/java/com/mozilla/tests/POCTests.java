3d4421933c9a4ac90019bf3b2fd90c015be1f761
vkorolM <vkorolM@mozilla.com>
2023-06-21 00:17:00 -0400

    refactor

---
+++ src/test/java/com/mozilla/tests/POCTests.java
@@ -0,0 +1,12 @@
+package com.mozilla.tests;
+
+import com.mozilla.core.BaseTest;
+import org.testng.annotations.Test;
+
+public class POCTests extends BaseTest {
+
+		@Test
+		public void firstTest() {
+				System.out.println("Test");
+		}
+}
